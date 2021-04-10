d7ce29c3e77a97947da664bf6a908b5054a2b82b
Gaurav <goenkagaurav123@gmail.com>
2021-04-10 20:03:32 +0530

    services started

--- src/component/ContactUs.js
+++ src/component/ContactUs.js
@@ -19,7 +19,7 @@ function ContactUs() {
 
             <div className="row">
                 <div className="col-sm-12">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m22!1m8!1m3!1d3732.094054972724!2d77.01396021492727!3d20.7064049361739!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d20.7064235!2d77.01618739999999!4m5!1s0x3bd72f8f4b5a640f%3A0xf363bf9cd733d44c!2sdr%20sumit%20agrawal%20akola%20embed%20map!3m2!1d20.7063718!2d77.0161704!5e0!3m2!1sen!2sin!4v1618058020833!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    <Iframe src="https://www.google.com/maps/embed?pb=!1m22!1m8!1m3!1d3732.094054972724!2d77.01396021492727!3d20.7064049361739!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d20.7064235!2d77.01618739999999!4m5!1s0x3bd72f8f4b5a640f%3A0xf363bf9cd733d44c!2sdr%20sumit%20agrawal%20akola%20embed%20map!3m2!1d20.7063718!2d77.0161704!5e0!3m2!1sen!2sin!4v1618058020833!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"/>
                 </div>
             </div>
         </div>
---
+++ src/component/Services.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Services() {
+    return (
+        <div className="row">
+            <div className="col-sm-12 col-md-4">
+            <h3>Rooms</h3>
+            </div>
+
+            
+            <div className="col-sm-12 col-md-4">
+
+            </div>
+
+            
+            <div className="col-sm-12 col-md-4">
+
+            </div>
+        </div>
+    )
+}
+
+export default Services
